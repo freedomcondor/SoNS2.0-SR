64863d644cf37e1fadb853d0af565816de79670f
Weixu Zhu <freedomcondor@126.com>
2023-07-03 11:34:13 +0200

    Add all violin plot datas for supp material

--- src/scripts/generateViolinPlot_sup.in.py
+++ src/scripts/generateViolinPlot_sup.in.py
@@ -15,17 +15,43 @@ def readDataFromFolder(dataFolder) :
 folders = [
 	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_01_formation_1_2d_10p/data_hw/data",
 	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_01_formation_1_2d_10p/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_10_formation_1_2d_6p_group_start/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_10_formation_1_2d_6p_group_start/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_02_obstacle_avoidance_small/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_02_obstacle_avoidance_small/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_03_obstacle_avoidance_large/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_03_obstacle_avoidance_large/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_04_switch_line/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_04_switch_line/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_05_gate_switch/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_05_gate_switch/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_09_1d_switch_rescue/data_hw",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_09_1d_switch_rescue/data_simu/data",
+
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_08_split/data_hw/data",
+	"@CMAKE_MNS_DATA_PATH@/src/experiments/exp_0_hw_08_split/data_simu/data",
 ]
 
 datas = []
+index = 0
 for folder in folders :
+	print("reading : " + folder)
 	data = readDataFromFolder(folder)
 	datas.append(data)
 
-U, p1 = mannwhitneyu(datas[0], datas[1])
-print("U = ", U)
-T, p1 = ttest_ind(datas[0], datas[1])
-print("T = ", T)
+	if index % 2 == 1 :
+		U, p1 = mannwhitneyu(datas[index-1], datas[index])
+		print("U = ", U)
+		T, p1 = ttest_ind(datas[index-1], datas[index])
+		print("T = ", T)
+
+	index = index + 1
 
 fig, ax = plt.subplots(1, 1)
 
