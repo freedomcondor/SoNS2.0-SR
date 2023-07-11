24a1f0148758edefe337e2408cd18240222f7e0d
Weixu Zhu <zhuweixu_harry@126.com>
2023-07-11 23:23:13 +0200

    Add all data sets to the list in public data generator

--- src/scripts/generatePublicData.in.sh
+++ src/scripts/generatePublicData.in.sh
@@ -123,42 +123,112 @@ copy_exp_folder() {
 #-----------------------------------------------------------------------------------
 # list of data sets and their public names
 data_set_lists=(
-	"exp_0_hw_01_formation_1_2d_10p/data_hw/data    1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Real_robots_experiments"
-	"exp_0_hw_01_formation_1_2d_10p/data_simu/data  1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Simulation_experiments/12robots"
+#	"exp_0_hw_01_formation_1_2d_10p/data_hw/data    1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Real_robots_experiments"
+#	"exp_0_hw_01_formation_1_2d_10p/data_simu/data  1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Simulation_experiments/12robots"
+#	"exp_1_simu_01_formation_10d/data_simu/data     1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Simulation_experiments/50robots"
+
+#	"exp_0_hw_10_formation_1_2d_6p_group_start/data_hw/data     1_Mission_Self-organized_hierarchy/Variant1_Clustered_start/Real_robots_experiments"
+#	"exp_0_hw_10_formation_1_2d_6p_group_start/data_simu/data   1_Mission_Self-organized_hierarchy/Variant1_Clustered_start/Simulation_experiments/8robots"
+#	"exp_1_simu_10_formation_10d_group_start/data_simu/data     1_Mission_Self-organized_hierarchy/Variant1_Clustered_start/Simulation_experiments/50robots"
+
+#	"exp_0_hw_02_obstacle_avoidance_small/data_hw/data          2_Mission_Global_local_goals/Variant1_Smaller_denser_obstacles/Real_robots_experiments"
+#	"exp_0_hw_02_obstacle_avoidance_small/data_simu/data        2_Mission_Global_local_goals/Variant1_Smaller_denser_obstacles/Simulation_experiments/8robots"
+#	"exp_1_simu_02_obstacle_avoidance_small_10d/data_simu/data  2_Mission_Global_local_goals/Variant1_Smaller_denser_obstacles/Simulation_experiments/50robots"
+
+#	"exp_0_hw_03_obstacle_avoidance_large/data_hw/data          2_Mission_Global_local_goals/Variant2_Larger_less_dense_obstacles/Real_robots_experiments"
+#	"exp_0_hw_03_obstacle_avoidance_large/data_simu/data        2_Mission_Global_local_goals/Variant2_Larger_less_dense_obstacles/Simulation_experiments/8robots"
+#	"exp_1_simu_03_obstacle_avoidance_large_10d/data_simu/data  2_Mission_Global_local_goals/Variant2_Larger_less_dense_obstacles/Simulation_experiments/50robots"
+
+#	"exp_0_hw_04_switch_line/data_hw/data                       3_Mission_Collective_sensing_actuation/Real_robots_experiments"
+#	"exp_0_hw_04_switch_line/data_simu/data                     3_Mission_Collective_sensing_actuation/Simulation_experiments/8robots"
+#	"exp_1_simu_04_switch_line/data_simu/data                   3_Mission_Collective_sensing_actuation/Simulation_experiments/50robots"
+
+#	"exp_0_hw_05_gate_switch/data_hw/data                       4_Mission_Binary_decision/Real_robots_experiments"
+#	"exp_0_hw_05_gate_switch/data_simu/data                     4_Mission_Binary_decision/Simulation_experiments/8robots"
+#	"exp_2_simu_scalability/data_simu_scale_2/data              4_Mission_Binary_decision/Simulation_experiments/65robots"
+
+#	"exp_0_hw_08_split/data_hw/data                    5_Mission_Splitting_merging/Variant1_Search_and_rescue_in_passage/Real_robots_experiments"
+#	"exp_0_hw_08_split/data_simu/data                  5_Mission_Splitting_merging/Variant1_Search_and_rescue_in_passage/Simulation_experiments/8robots"
+#	"exp_1_simu_08_split/data_simu/data                5_Mission_Splitting_merging/Variant1_Search_and_rescue_in_passage/Simulation_experiments/50robots"
+
+#	"exp_0_hw_09_1d_switch_rescue/data_hw              5_Mission_Splitting_merging/Variant2_Push_away_obstruction/Real_robots_experiments"
+#	"exp_0_hw_09_1d_switch_rescue/data_simu/data       5_Mission_Splitting_merging/Variant2_Push_away_obstruction/Simulation_experiments/5robots"
+
+#	"exp_2_simu_scalability/data_simu_scale_1/data     6_Scalability/Scalability_in_decision_making_mission/35robots"
+#	"exp_2_simu_scalability/data_simu_scale_2/data     6_Scalability/Scalability_in_decision_making_mission/65robots"
+#	"exp_2_simu_scalability/data_simu_scale_3/data     6_Scalability/Scalability_in_decision_making_mission/95robots"
+#	"exp_2_simu_scalability/data_simu_scale_4/data     6_Scalability/Scalability_in_decision_making_mission/125robots"
+
+#----------------------------
+
+#	"exp_0_hw_07_fault_tolerance/data_hw/data                         7_Fault_tolerance/Real_robots_experiments"
+#	"exp_0_hw_07_fault_tolerance/data_simu/data                       7_Fault_tolerance/Simulation_experiments/8robots"
+
+#	"exp_3_simu_02_fault_tolerance_33/data_simu/data                  7_Fault_tolerance/Simulation_experiments/50robots/Variant1_One_third_chance_for_each_robot_to_fail"
+#	"exp_3_simu_02_fault_tolerance_33/data_simu_drone_failure/data    7_Fault_tolerance/Simulation_experiments/50robots/Variants_additional/One_third_chance_for_each_aerial_robot_to_fail"
+#	"exp_3_simu_02_fault_tolerance_33/data_simu_pipuck_failure/data   7_Fault_tolerance/Simulation_experiments/50robots/Variants_additional/One_third_chance_for_each_ground_robot_to_fail"
+
+#	"exp_3_simu_01_fault_tolerance_66/data_simu/data                  7_Fault_tolerance/Simulation_experiments/50robots/Variant2_Two_thirds_chance_for_each_robot_to_fail"
+#	"exp_3_simu_01_fault_tolerance_66/data_simu_drone_failure/data    7_Fault_tolerance/Simulation_experiments/50robots/Variants_additional/Two_thirds_chance_for_each_aerial_robot_to_fail"
+#	"exp_3_simu_01_fault_tolerance_66/data_simu_pipuck_failure/data   7_Fault_tolerance/Simulation_experiments/50robots/Variants_additional/Two_thirds_chance_for_each_ground_robot_to_fail"
+
+#	"exp_3_simu_04_fault_tolerance_visual/data_simu_0.5s/data         7_Fault_tolerance/Simulation_experiments/50robots/Variant3_Temporary_system-wide_vision_failure/0.5s_failure"
+#	"exp_3_simu_04_fault_tolerance_visual/data_simu_1s/data           7_Fault_tolerance/Simulation_experiments/50robots/Variant3_Temporary_system-wide_vision_failure/1s_failure"
+#	"exp_3_simu_04_fault_tolerance_visual/data_simu_30s/data          7_Fault_tolerance/Simulation_experiments/50robots/Variant3_Temporary_system-wide_vision_failure/30s_failure"
+
+#	"exp_3_simu_03_fault_tolerance_communication/data_simu_0.5s/data  7_Fault_tolerance/Simulation_experiments/50robots/Variant4_Temporary_system-wide_communication_failure/0.5s_failure"
+#	"exp_3_simu_03_fault_tolerance_communication/data_simu_1s/data    7_Fault_tolerance/Simulation_experiments/50robots/Variant4_Temporary_system-wide_communication_failure/1s_failure"
+#	"exp_3_simu_03_fault_tolerance_communication/data_simu_30s/data   7_Fault_tolerance/Simulation_experiments/50robots/Variant4_Temporary_system-wide_communication_failure/30s_failure"
+
+#	"exp_0_hw_06_formation_1_3d_14p/data_simu/data        8_Demos/Simulation_of_the_real_robot_demo_with_17robots_with_3drone"
+)
+
+special_run_list=(
+#	"exp_0_hw_07_fault_tolerance/datas_substitute/1_non_brain_drone_failure  8_Demos/Real_robots_with_failure_and_substitution/Non_brain_drone_failure"
+#	"exp_0_hw_07_fault_tolerance/datas_substitute/2_brain_drone_failure      8_Demos/Real_robots_with_failure_and_substitution/Brain_drone_failure"
+#	"exp_0_hw_07_fault_tolerance/datas_substitute/3_pipuck_failure           8_Demos/Real_robots_with_failure_and_substitution/Pipuck_failure"
 )
 
 copy_data_set_folder() {
 	input_data_set_folder=$1
 	output_data_set_folder=$2
+	echo "---------------------------------------------"
 	echo "copying from $input_data_set_folder"
 	echo "          to $output_data_set_folder"
 
 	if [ ! -d $raw_exps_dir/$input_data_set_folder ]; then
 		echo "[Waring]: input data set doesn't exist, skipped: $public_dir/$output_data_set_folder"
-		continue
+		return
 	fi
 
 	mkdir -p $public_dir/$output_data_set_folder
 	cd $raw_exps_dir/$input_data_set_folder
 	i=0
 	for run_folder in */; do
-		echo $run_folder
+		echo -n "        $run_folder   "
 		((i=i+1))
 		output_run_folder=$public_dir/$output_data_set_folder/$run_folder
 		if [[ $run_folder != run* ]]; then
-			echo "            " $run_folder is not run*, renaming to run$i
+			echo is not run*, renaming to run$i
 			output_run_folder=$public_dir/$output_data_set_folder/run$i/
 		fi
 
 		mkdir -p $output_run_folder
 		copy_run $raw_exps_dir/$input_data_set_folder/$run_folder $output_run_folder #$run_folder
 	done
+	echo "copy data set done"
 }
 #-----------------------------------------------------------------------------------
 
 for i in ${!data_set_lists[@]}; do
 	read -a exp_tuple <<< ${data_set_lists[$i]}
 	copy_data_set_folder ${exp_tuple[0]} ${exp_tuple[1]}
+done
+
+for i in ${!special_run_list[@]}; do
+	read -a exp_tuple <<< ${special_run_list[$i]}
+	mkdir -p $public_dir/${exp_tuple[1]}/
+	copy_run $raw_exps_dir/${exp_tuple[0]}/ $public_dir/${exp_tuple[1]}/
 done
 
 cd $ORIGIN_DIR
