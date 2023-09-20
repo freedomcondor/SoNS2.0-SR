2555c789eb5cd19fd2b5844671138258c2cb4bee
Weixu Zhu <zhuweixu_harry@126.com>
2023-09-20 21:16:02 +0200

    Adjust scripts for Mission1 Variant2 Large

--- src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/scripts/drawLine.in.py
+++ src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/scripts/drawLine.in.py
@@ -4,7 +4,7 @@ exec(compile(open(drawDataFileName, "rb").read(), drawDataFileName, 'exec'))
 
 cmake_source_dir         = "@CMAKE_SOURCE_DIR@"
 cmake_current_source_dir = "@CMAKE_CURRENT_SOURCE_DIR@"
-cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "")
+cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "").replace("/scripts", "")
 #cmake_relative_dir starts with / and end with no /
 DATADIR  = "@CMAKE_SoNS_DATA_PATH@" + cmake_relative_dir + "/"
 DATADIR += "data_simu/data"
--- src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/scripts/drawLine_sup_simu.in.py
+++ src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/scripts/drawLine_sup_simu.in.py
@@ -14,7 +14,7 @@ exec(compile(open(drawTrackLogFileName, "rb").read(), drawTrackLogFileName, 'exe
 
 cmake_source_dir         = "@CMAKE_SOURCE_DIR@"
 cmake_current_source_dir = "@CMAKE_CURRENT_SOURCE_DIR@"
-cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "")
+cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "").replace("/scripts", "")
 #cmake_relative_dir starts with / and end with no /
 dataFolder  = "@CMAKE_SoNS_DATA_PATH@" + cmake_relative_dir + "/"
 dataFolder += "data_simu/data"
