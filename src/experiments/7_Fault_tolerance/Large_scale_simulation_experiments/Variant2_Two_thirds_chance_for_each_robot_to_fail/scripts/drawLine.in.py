37411dbbb8695069e5f19951d94573907f53105d
Weixu Zhu <zhuweixu_harry@126.com>
2023-09-21 16:45:08 +0200

    Upgrade scripts for Mission7 LargeScale Var2

--- src/experiments/7_Fault_tolerance/Large_scale_simulation_experiments/Variant2_Two_thirds_chance_for_each_robot_to_fail/scripts/drawLine.in.py
+++ src/experiments/7_Fault_tolerance/Large_scale_simulation_experiments/Variant2_Two_thirds_chance_for_each_robot_to_fail/scripts/drawLine.in.py
@@ -2,9 +2,49 @@ drawDataFileName = "@CMAKE_SOURCE_DIR@/scripts/drawData.py"
 #execfile(drawDataFileName)
 exec(compile(open(drawDataFileName, "rb").read(), drawDataFileName, 'exec'))
 
-for subfolder in getSubfolders("@CMAKE_CURRENT_SOURCE_DIR@/../data") :
-	drawData(readDataFrom(subfolder + "result_data.txt"))
+# -----------------------------
+hw_or_simu = None
 
-#drawData(readDataFrom("result_data.txt"))
+import sys
+params = sys.argv
+if len(sys.argv) >= 2 :
+	if sys.argv[1] == "hw" :
+		hw_or_simu = "hw"
+	if sys.argv[1] == "simu" :
+		hw_or_simu = "simu"
+
+if hw_or_simu == None :
+	print("please specify hw or simu")
+	exit()
+# -----------------------------
+cmake_source_dir         = "@CMAKE_SOURCE_DIR@"
+cmake_current_source_dir = "@CMAKE_CURRENT_SOURCE_DIR@"
+cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "").replace("/scripts", "")
+#cmake_relative_dir starts with / and end with no /
+DATADIR  = "@CMAKE_SoNS_DATA_PATH@" + cmake_relative_dir + "/"
+if hw_or_simu == "hw" :
+	DATADIR+="data_hw/data"
+else :
+	DATADIR+="data_simu/data"
+# -----------------------------
+for subFolder in getSubfolders(DATADIR) :
+	# choose a specific run
+	#if subFolder != DATADIR+ "/run1/" :
+	#	continue
+
+	data = readDataFrom(subFolder + "result_data.txt")
+#	if data[120] > 1.3 :
+#		print(subFolder)
+	drawData(data)
+	print("length: ", len(data), ":", subFolder)
+	drawData(readDataFrom(subFolder + "result_lowerbound_data.txt"))
+	'''
+	for subFile in getSubfiles(subFolder + "result_each_robot_error_data") :
+		data = readDataFrom(subFile)
+		if data[115] < 0.2 :
+			print(subFile)
+		drawData(data)
+	break
+	'''
 
 plt.show()
