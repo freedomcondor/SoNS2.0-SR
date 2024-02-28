a698133c6235050782159f43d1d5ce44d1b9a62f
Weixu Zhu <zhuweixu_harry@126.com>
2024-02-28 14:05:58 +0800

    Update and re-run Mission1 Variant2 Large scale to remove split cases

--- src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/run.in.py
+++ src/experiments/1_Mission_Self-organized_hierarchy/Variant2_Scattered_start/Large_scale_simulation_experiments/run.in.py
@@ -8,15 +8,15 @@ import os
 drone_locations = generate_random_locations(10,                  # total number
                                             0.5, 0,             # origin location
                                             -4, 4,              # random x range
-                                            -4, 4,              # random y range
-                                            1.5, 1.6)           # near limit and far limit
+                                            -3, 3,              # random y range
+                                            1.3, 1.4)           # near limit and far limit
 pipuck_locations = generate_slave_locations_with_origin(
                                             40,
                                             drone_locations,
                                             1, 0.7,
                                             -4, 4,              # random x range
-                                            -4.0, 4.0,          # random y range
-                                            0.5, 0.8)           # near limit and far limit
+                                            -3, 3,          # random y range
+                                            0.3, 0.7)           # near limit and far limit
 drone_xml = generate_drones(drone_locations, 1)                 # from label 1 generate drone xml tags
 
 pipuck_locations.remove(pipuck_locations[0])
@@ -43,6 +43,9 @@ generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/sons_template.argos",
               my_type="pipuck"
               stabilizer_preference_robot="pipuck1"
               stabilizer_preference_brain="drone1"
+
+              safezone_pipuck_pipuck="0.3"
+              driver_spring_default_speed_scalar="6"
         ''')],
         ["DRONE_CONTROLLER", generate_drone_controller('''
               script="@CMAKE_CURRENT_BINARY_DIR@/simu/common.lua"
@@ -53,6 +56,9 @@ generate_argos_file("@CMAKE_CURRENT_BINARY_DIR@/sons_template.argos",
               drone_default_height="1.8"
               drone_default_start_height="1.8"
               dangerzone_drone="1.3"
+
+              safezone_pipuck_pipuck="0.3"
+              driver_spring_default_speed_scalar="6"
         ''')],
         ["SIMULATION_SETUP",  generate_physics_media_loop_visualization("@CMAKE_BINARY_DIR@")],
     ]
