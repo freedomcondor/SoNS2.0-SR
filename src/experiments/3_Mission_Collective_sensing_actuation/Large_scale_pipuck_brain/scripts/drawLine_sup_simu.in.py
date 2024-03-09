c57799d893e56afbbf51d58eed272ff289374dae
Weixu Zhu <zhuweixu_harry@126.com>
2024-03-09 19:44:07 +0800

    Add a pipuck spine scenario to Mission3 tunnel

---
+++ src/experiments/3_Mission_Collective_sensing_actuation/Large_scale_pipuck_brain/scripts/drawLine_sup_simu.in.py
@@ -0,0 +1,85 @@
+drawDataFileName = "@CMAKE_SOURCE_DIR@/scripts/drawData.py"
+#execfile(drawDataFileName)
+exec(compile(open(drawDataFileName, "rb").read(), drawDataFileName, 'exec'))
+
+drawSRFigFileName = "@CMAKE_SOURCE_DIR@/scripts/drawSRFig_sup.py"
+#execfile(drawSRFigFileName)
+exec(compile(open(drawSRFigFileName, "rb").read(), drawSRFigFileName, 'exec'))
+
+logGeneratorFileName = "@CMAKE_SOURCE_DIR@/scripts/logReader/logReplayer.py"
+exec(compile(open(logGeneratorFileName, "rb").read(), logGeneratorFileName, 'exec'))
+
+drawTrackLogFileName = "@CMAKE_SOURCE_DIR@/scripts/drawTrackLogs_sup.py"
+exec(compile(open(drawTrackLogFileName, "rb").read(), drawTrackLogFileName, 'exec'))
+
+cmake_source_dir         = "@CMAKE_SOURCE_DIR@"
+cmake_current_source_dir = "@CMAKE_CURRENT_SOURCE_DIR@"
+cmake_relative_dir       = cmake_current_source_dir.replace(cmake_source_dir, "").replace("/scripts", "")
+#cmake_relative_dir starts with / and end with no /
+dataFolder  = "@CMAKE_SoNS_DATA_PATH@" + cmake_relative_dir + "/"
+dataFolder += "data_simu/data"
+
+savePDFNameBase = "Mission3_Collective_sensing_actuation_Large_scale_Simulation_"
+sampleList = [
+	"run13",
+	"run23",
+	"run35",
+	"run40",
+	"run50",
+]
+
+#----------------------------------
+track_option_base = {
+	'brain_marker'      :    '@CMAKE_SOURCE_DIR@/scripts/brain-icon-small.svg',
+
+	'dataFolder'        :    dataFolder,
+#	'sample_run'        :    sample_run,
+
+#	'trackLog_save'     : savePDFNameBase + sample_run + ".pdf",
+	'trackLog_show'     : False,
+
+	'x_lim'     :  [-12, 12]    ,
+	'y_lim'     :  [-12, 12]        ,
+	'z_lim'     :  [-4.0,8.0]    ,
+
+	'showRobotName'     : False,
+
+	'colored_key_frame' : True,
+
+	'SRFig_show'        : False,
+	'no_violin'         : True,
+	'main_ax_lim'            : [-0.5, 7.5],
+}
+
+#----------------------------------
+key_frame_list = {}
+key_frame_parent_index_list = {}
+x_lim_list = {}
+y_lim_list = {}
+z_lim_list = {}
+
+key_frame_example = [0, 500]
+
+#-----------------------------------------------------------
+for sample_run in sampleList :
+	track_option = track_option_base.copy()
+	track_option['sample_run'] = sample_run
+	track_option['trackLog_save'] = savePDFNameBase + sample_run + "_TrackLog.pdf"
+	if sample_run in key_frame_list:
+		track_option['key_frame'] = key_frame_list[sample_run]
+	else :
+		track_option['key_frame'] = key_frame_example
+	if sample_run in key_frame_parent_index_list :
+		track_option['key_frame_parent_index'] = key_frame_parent_index_list[sample_run]
+
+	if sample_run in x_lim_list :
+		track_option['x_lim'] = x_lim_list[sample_run]
+	if sample_run in y_lim_list :
+		track_option['y_lim'] = y_lim_list[sample_run]
+	if sample_run in z_lim_list :
+		track_option['z_lim'] = z_lim_list[sample_run]
+
+	drawTrackLog(track_option)
+
+	track_option['SRFig_save'] = savePDFNameBase + sample_run + "_ErrorLog.pdf"
+	drawSRFig(track_option)
